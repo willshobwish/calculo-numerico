bcb2d9f5162d00d229eecf363aeba5a1d1f8772d
Willian Murayama <willianyos13@gmail.com>
2024-04-10 19:02:25 -0300

    Feat: decomposição LU

--- 2024-03-27/01-exemplo.py
+++ 2024-03-27/01-exemplo.py
@@ -1,16 +1,44 @@
 import numpy as np
 
 
-def Sistema_triangular_superior(matriz_a: list, vetor_b: list):
-    n = len(matriz_a)
-    x = np.zeros(n)
-    x[n] = vetor_b[n] / matriz_a[n][n]
-    for k in range(1, n):
-        somatorio = 0
-        for i in range((n - k) + 1, n):
-            somatorio += matriz_a[n - k][i] * x[i]
-        x[n - k] = (vetor_b[n - k] - somatorio) / matriz_a[n - k][n - k]
-    return x
+def LUdecomposition(matrix_a: list):
+    m, n = np.shape(matrix_a)
+    matrix_zero = np.zeros(shape=(m, n))
+    identity_matrix = np.eye(N=n, M=m)
+
+    for index_i, element_i in enumerate(matrix_a):
+        for index_j, element_j in enumerate(matrix_a[index_i]):
+            if index_i <= index_j:
+                if index_i == 1:
+                    matrix_zero[index_i][index_j] = matrix_a[index_i][index_j]
+                else:
+                    somatorio = 0
+                    for index_k in range(0, index_i):
+                        somatorio += np.dot(
+                            identity_matrix[index_i][index_k],
+                            matrix_zero[index_k][index_j],
+                        )
+                    matrix_zero[index_i][index_j] = (
+                        matrix_a[index_i][index_j] - somatorio
+                    )
+            else:
+                if index_j == 1:
+                    identity_matrix[index_i][index_j] = (
+                        matrix_a[index_i][index_j] / matrix_zero[index_j][index_j]
+                    )
+                else:
+                    somatorio = 0
+                    for index_k in range(0, index_j):
+                        somatorio += np.dot(
+                            identity_matrix[index_i][index_k],
+                            matrix_zero[index_k][index_j],
+                        )
+                    identity_matrix[index_i][index_j] = (
+                        matrix_a[index_i][index_j] - somatorio
+                    ) / matrix_zero[index_j][index_j]
+    print(f"Matriz nula:\n{matrix_zero}\n")
+    print(f"Matriz identidade:\n{identity_matrix}\n")
+    print(f"Matriz A:\n{matrix_a}\n")
 
 
-print(Sistema_triangular_superior([[2, 1, 3], [0, 1, -1], [0, 0, 2]], [11, 1, 4]))
+LUdecomposition([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
