648cd14c9e6b85475a49678daab0f809355b78ca
Willian Murayama <willianyos13@gmail.com>
2024-04-17 21:32:39 -0300

    Feat: sistema triangular inferior

--- 2024-04-10/00-sistema-triangular-inferior.py
+++ 2024-04-10/00-sistema-triangular-inferior.py
@@ -1,7 +1,17 @@
 import numpy as np
 
 
-def sistema_triangular_inferior(matrix_A):
-    n, m = np.shape(matrix_A)
-    zero_matrix = np.zeros((n, 1))
-    zero_matrix[1] = 
+def sistema_triangular_inferior(matrix, b):
+    matrix = np.array(matrix)
+    x = np.zeros((len(matrix)))
+    x[0] = b[0] / matrix[0][0]
+    for k in range(1, len(matrix)):
+        somatorio = 0
+        for i in range(0, k):
+            somatorio += matrix[k][i] * x[i]
+
+        x[k] = (b[k] - somatorio) / matrix[k][k]
+    print(f"Matriz:\n{matrix}\n\nVetor:\n{x}\n")
+
+
+sistema_triangular_inferior([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3])
