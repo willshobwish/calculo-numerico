17bcf298f8568f32dc1663db4c273357f2abdfbd
Willian Murayama <willianyos13@gmail.com>
2024-04-18 23:44:50 -0300

    Fix: iteração linear

--- 2024-03-06/03-interacao-linear.py
+++ 2024-03-06/03-interacao-linear.py
@@ -1,23 +1,67 @@
-from math import sqrt, log10
+import math
 
 
 def function_1(x):
-    return sqrt(x + 2)
+    return math.sqrt(x + 2)
 
 
-x = 2.2
-tolerance = 10**-3
-error = 1
-iteration = 0
-xbar = 2
-while error > tolerance and iteration < 10:
-    new_x = function_1(x)
-    xp1 = abs(function_1(new_x))
-    error = abs((new_x - x)) / abs(new_x)
+def question_1(x):
+    return math.exp(x)/4
 
-    errork1 = abs(xp1 - xbar)
-    error_k1 = abs(x - 2)
-    x = new_x
-    p = log10(errork1 / error) / log10(error / error_k1)
-    print(new_x, p)
-    iteration += 1
+
+def question_2(x):
+    return 2 * x * x - 5 * x + 2
+
+
+def iteracao_linear(function, x, x_1, xbar, tolerance, error, max_iteration, files):
+    iteration = 0
+    resultados = []
+    while error > tolerance and iteration < max_iteration:
+        x = function(x_1)
+        x1 = function(x)
+        error = abs(x - x_1) / abs(x)
+        error2 = abs(x1 - xbar)
+        error1 = abs(x - xbar)
+        error_1 = abs(x_1 - xbar)
+        p = math.log(error2 / error1) / math.log(error1 / error_1)
+        print(f"{x} {p}")
+        resultados.append(x)
+        x_1 = x
+        x = x1
+        iteration += 1
+
+    string_array = [str(element) for element in resultados]
+    with open(f"{files}", "w") as file_file:
+        file_file.write("\n".join(string_array))
+
+
+iteracao_linear(
+    function=question_1,
+    x=0,
+    x_1=1,
+    xbar=2,
+    tolerance=10**-2,
+    error=1,
+    max_iteration=20,
+    files="Questão 1_1.txt",
+)
+iteracao_linear(
+    function=question_1,
+    x=2,
+    x_1=4,
+    xbar=2,
+    tolerance=10**-2,
+    error=1,
+    max_iteration=20,
+    files="Questão 1_2.txt",
+)
+iteracao_linear(
+    function=question_2,
+    x=0.4,
+    x_1=2,
+    xbar=5,
+    tolerance=10**-2,
+    error=1,
+    max_iteration=20,
+    files="Questão 2.txt",
+)
