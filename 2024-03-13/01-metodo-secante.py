c1a2e317f3f3f8a01b6e2b9ecb91a512954d41c9
Willian Murayama <willianyos13@gmail.com>
2024-04-19 09:15:05 -0300

    Feat: questions

--- 2024-03-13/01-metodo-secante.py
+++ 2024-03-13/01-metodo-secante.py
@@ -17,6 +17,22 @@ def function_4(x):
     return math.exp(-x) - math.log10(x)
 
 
+def question_10(x):
+    return math.sqrt(x) - 5 * math.exp(-x)
+
+
+def question_11a(x):
+    return -2.7 * math.log(x) - x
+
+
+def question_11b(x):
+    return math.log10(x) - math.cos(x)
+
+
+def question_11c(x):
+    return math.exp(-x) - math.log10(x)
+
+
 def secant_method(function, xk, xk_1, tolerance, max_iteration):
     error1 = error_1 = error = 10 * tolerance
     index = 0
@@ -36,12 +52,32 @@ def secant_method(function, xk, xk_1, tolerance, max_iteration):
     print(f"Convergencia: {convergence}\n")
 
 
+# secant_method(
+#     function=function_1, xk=1.4, xk_1=1.5, tolerance=10**-2, max_iteration=100
+# )
+
+# secant_method(function=function_2, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+
+# secant_method(function=function_3, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+
+# secant_method(function=function_4, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+
 secant_method(
-    function=function_1, xk=1.4, xk_1=1.5, tolerance=10**-2, max_iteration=100
+    function=question_10, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100
 )
 
-secant_method(function=function_2, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+secant_method(
+    function=question_11a, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100
+)
 
-secant_method(function=function_3, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+secant_method(
+    function=question_11b, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100
+)
 
-secant_method(function=function_4, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100)
+secant_method(
+    function=question_11c, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100
+)
+
+# secant_method(
+#     function=question_10, xk=1, xk_1=1.25, tolerance=10**-4, max_iteration=100
+# )
--- 2024-03-13/02-regula-falsi.py
+++ 2024-03-13/02-regula-falsi.py
@@ -7,15 +7,16 @@ def function_1(x):
 
 def regula_falsi(function, xk, xk_1, tolerance, max_iteration):
 
-    error = error_1 = error1 = 10 * tolerance
+    error = error_1 = error1 = 1
     cont = 1
     print(function.__name__)
     while error > tolerance and cont < max_iteration:
+        error_1 = error
+        error = error1
         xk1 = (xk_1 * function(xk) - xk * function(xk_1)) / (
             function(xk) - function(xk_1)
         )
-        error_1 = error
-        error = error1
+
         error1 = abs(xk1 - xk) / abs(xk1)
         if function(xk) * function(xk_1) < 0:
             xk_1 = xk
@@ -34,9 +35,19 @@ def regula_falsi(function, xk, xk_1, tolerance, max_iteration):
 # max_iteration = 100
 
 
-def function_2(x):
+def question_12(x):
     return x - math.cos(x)
 
 
+def question_13a(x):
+    return math.sin(x) - x * math.exp(x)
+
+
+def question_13b(x):
+    return math.cos(x) - math.exp(x)
+
+
 regula_falsi(function=function_1, xk=0, xk_1=0.5, tolerance=10**-4, max_iteration=40)
-regula_falsi(function=function_2, xk=-1, xk_1=1, tolerance=10**-5, max_iteration=40)
+regula_falsi(function=question_12, xk=-1, xk_1=1, tolerance=10**-5, max_iteration=40)
+regula_falsi(function=question_13a, xk=-4, xk_1=3, tolerance=10**-5, max_iteration=40)
+regula_falsi(function=question_13b, xk=-2, xk_1=-1, tolerance=10**-5, max_iteration=40)
